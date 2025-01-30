035126b3007306250256692bc1017db892dca53b
J4BC97 <hammerban123@gmail.com>
2025-01-30 18:20:51 -0500

    Actualizacion Commit: TaskManager App

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import {router} from './routes/authRoutes.js'; // Asegúrate de importar correctamente
+import { router } from './routes/authRoutes.js'; // Asegúrate de importar correctamente
 
 dotenv.config();
 const app = express();
@@ -10,3 +10,7 @@ app.use('/auth', router); // Usa las rutas de autenticación
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
+
+app.get('/', (req, res) => {
+    res.send('API funcionando');
+});
